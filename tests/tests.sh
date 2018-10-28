e46f2bc926d3718fe6207bbf94e8c239c9f1e15d
Tino Reichardt <milky-7zip@mcmilk.de>
2018-10-28 09:02:56 +0100

    test it

--- tests/tests.sh
+++ tests/tests.sh
@@ -14,12 +14,11 @@ dparam="-mmt=2"
 
 rm -f *.log
 m="zstd"
-for l in `seq 1 22`; do
+for l in `seq 1 3`; do
   echo wtime 7z a test.7z -m0=$m -ms=on -mmt=4 -mx$l mcorpus.tar 2>>$m_mx$l.log
   wtime.exe 7z.exe a test.7z $cparam -m0=$m -mx$l mcorpus.tar 2>>${m}_mx${l}_c.log
   wtime.exe 7z.exe t test.7z $dparam 2>>${m}_mx${l}_d.log
   rm -f test.7z
 done
 
-cd $APPVEYOR_BUILD_FOLDER
-7z a logs.7z tests\*.log
+7z a ../logs.7z *.log
