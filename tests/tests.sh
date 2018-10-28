ee9de8947684845dd489c457fac8641a41f9849f
Tino Reichardt <milky-7zip@mcmilk.de>
2018-10-28 02:12:13 +0200

    test8

--- tests/tests.sh
+++ tests/tests.sh
@@ -6,15 +6,16 @@ zcat cache/mcorpus.tar.gz > mcorpus.tar
 pwd
 ls -l
 
-cparam="-ms=on -mmt=4"
-dparam="-mmt=4"
+cparam="-ms=on -mmt=2"
+dparam="-mmt=2"
 
 m="zstd"
 for l in `seq 1 22`; do
-  echo wtime 7z a test.7z -mm=$m -ms=on -mmt=4 -mx$l mcorpus.tar 2>>$m_mx$l.log
-  wtime 7z a test.7z $cparam -mm=$m -mx$l mcorpus.tar 2>>${m}_mx${l}_c.log
+  echo wtime 7z a test.7z -m0=$m -ms=on -mmt=4 -mx$l mcorpus.tar 2>>$m_mx$l.log
+  wtime 7z a test.7z $cparam -m0=$m -mx$l mcorpus.tar 2>>${m}_mx${l}_c.log
   wtime 7z t test.7z $dparam 2>>${m}_mx${l}_d.log
   rm -f test.7z
 done
 
-7z a logs.7z *.log
+cd %APPVEYOR_BUILD_FOLDER%
+7z a logs.7z tests\*.log
