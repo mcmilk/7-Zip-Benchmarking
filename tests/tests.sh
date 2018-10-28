dbd198ec664a3fec50f787da8d9a69cda3e02276
Tino Reichardt <milky-7zip@mcmilk.de>
2018-10-28 02:27:05 +0200

    test it

--- tests/tests.sh
+++ tests/tests.sh
@@ -15,8 +15,8 @@ rm -f *.log
 m="zstd"
 for l in `seq 1 22`; do
   echo wtime 7z a test.7z -m0=$m -ms=on -mmt=4 -mx$l mcorpus.tar 2>>$m_mx$l.log
-  ./wtime 7z a test.7z $cparam -m0=$m -mx$l mcorpus.tar 2>>${m}_mx${l}_c.log
-  ./wtime 7z t test.7z $dparam 2>>${m}_mx${l}_d.log
+  ./wtime 7z.exe a test.7z $cparam -m0=$m -mx$l mcorpus.tar 2>>${m}_mx${l}_c.log
+  ./wtime 7z.exe t test.7z $dparam 2>>${m}_mx${l}_d.log
   rm -f test.7z
 done
 
